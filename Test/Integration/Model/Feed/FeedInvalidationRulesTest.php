0c074bea02e26a2089bac61c84c3c69d353758a3
Sental <markrees90@gmail.com>
2026-09-18 16:48:25 +0200

    fix: remove whitespace to satisify coding standard

--- Test/Integration/Model/Feed/FeedInvalidationRulesTest.php
+++ Test/Integration/Model/Feed/FeedInvalidationRulesTest.php
@@ -128,7 +128,7 @@ class FeedInvalidationRulesTest extends TestCase
      * CMS page saves queue hreflang only for URL-relevant changes.
      *
      * @return void
-     * 
+     *
      * @magentoDataFixture Magento/Cms/_files/pages.php
      */
     #[DataFixture(StoreFixture::class, as: 'second_store')]
